3f49b5662eaed8e43e7853cb1a54ae8654f4196a
Keertana Chidambaram <43830897+keertanavc@users.noreply.github.com>
2024-09-18 15:40:48 -0700

    Update train.py

--- train.py
+++ train.py
@@ -46,7 +46,8 @@ def worker_main(rank: int, world_size: int, config: DictConfig, policy: nn.Modul
 
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
-def train_weighted_dpo(config: DictConfig):
+# def train_weighted_dpo(config: DictConfig):
+def main(config: DictConfig):
     """Main entry point for training. Validates config, creates/initializes model(s), and kicks off worker process(es)."""
 
     # Resolve hydra references, e.g. so we don't re-compute the run directory
