18d66a5d56c986afb42322d570233cf8dc615021
Keertana Chidambaram <43830897+keertanavc@users.noreply.github.com>
2024-09-22 22:46:22 -0700

    Update preference_datasets.py

--- preference_datasets.py
+++ preference_datasets.py
@@ -428,7 +428,9 @@ def get_batch_iterator(names: List[str],
                     if include_weight:
                         indx = int(min(p[0], p[1])/2)
                         print(indx)
+                        print(weight)
                         print(weight[indx])
+                        print(human_label)
                         print(human_label[indx])
                         batch_element = tokenize_batch_element(prompt, responses[p[0]], responses[p[1]], truncation_mode, tokenizer, max_length, max_prompt_length, weight[indx], human_label[indx])
                     else:
