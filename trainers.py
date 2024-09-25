66c05fd51f084739a6da5b7ba048a4aab5c31d9b
Keertana Chidambaram <43830897+keertanavc@users.noreply.github.com>
2024-09-24 22:54:23 -0700

    Update trainers.py

--- trainers.py
+++ trainers.py
@@ -509,9 +509,10 @@ class BasicTrainer(object):
         print('local value', self.rank)
         print(local_numerator, self.rank)
         total_numerator = all_gather_if_needed(local_numerator, self.rank, self.world_size)
-        total_numerator = torch.sum(total_numerator, dim=0)
+        # total_numerator = torch.sum(total_numerator, dim=0)
         print('global value', self.rank)
         print(total_numerator, self.rank)
+        print(total_numerator.shape, self.rank)
         assert total_numerator.shape == local_numerator.shape
 
         self.log_numerator_gamma[self.group, :] = torch.log(torch.tensor(self.eta[self.group]))
