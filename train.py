59262b7c20f55e27c7174f82a92dfeccc01d0ba1
Keertana Chidambaram <43830897+keertanavc@users.noreply.github.com>
2024-09-24 22:44:15 -0700

    Update train.py

--- train.py
+++ train.py
@@ -28,22 +28,23 @@ def worker_main(rank: int, world_size: int, config: DictConfig, policy: nn.Modul
         wandb.init = lambda *args, **kwargs: None
         wandb.log = lambda *args, **kwargs: None
 
-    # if rank == 0 and config.wandb.enabled:
-    #     os.environ['WANDB_CACHE_DIR'] = get_local_dir(config.local_dirs)
-    #     wandb.init(
-    #         entity=config.wandb.entity,
-    #         project=config.wandb.project,
-    #         config=OmegaConf.to_container(config),
-    #         dir=get_local_dir(config.local_dirs),
-    #         name=config.exp_name,
-    #     )
+    if rank == 0 and config.wandb.enabled:
+        os.environ['WANDB_CACHE_DIR'] = get_local_dir(config.local_dirs)
+        wandb.init(
+            entity=config.wandb.entity,
+            project=config.wandb.project,
+            config=OmegaConf.to_container(config),
+            dir=get_local_dir(config.local_dirs),
+            name=config.exp_name,
+        )
 
     TrainerClass = getattr(trainers, config.trainer)
     print(f'Creating trainer on process {rank} with world size {world_size}')
     trainer = TrainerClass(policy, config, config.seed, config.local_run_dir, reference_model=reference_model, rank=rank, world_size=world_size, dynamic_params=dynamic_params)
     trainer.train()
-    trainer.save()
-
+    if rank == 0:
+        dynamic_params['etas'], dynamic_params['gammas'] = trainer.update_eta_gamma()
+    # trainer.save()
 
 # @hydra.main(version_base=None, config_path="config", config_name="config")
 def train_weighted_dpo(config: DictConfig, dynamic_params: Dict = None):
