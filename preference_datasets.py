f388071e1ca37b59bf8104590dc7a0d49cde4c50
Keertana Chidambaram <43830897+keertanavc@users.noreply.github.com>
2024-09-27 23:59:04 -0700

    Update preference_datasets.py

--- preference_datasets.py
+++ preference_datasets.py
@@ -161,7 +161,8 @@ def get_hh(split: str, silent: bool = False, cache_dir: str = None) -> Dict[str,
 def get_imdb(split: str, name: str, silent: bool = False, cache_dir: str = None, weights_dict: Dict = None) -> Dict[str, Dict[str, Union[List[Tuple[int, int]], List[str], str]]]:
     # assign equal weight to all data points, i.e. perform regular DPO is no weights are passed
     print(f'Loading IMDb dataset ({split} split) from Huggingface...')
-    dataset = datasets.load_dataset("keertanavc/imdb_prefix20_forDPO_multi-preference_small", split=split, cache_dir=cache_dir)
+    # dataset = datasets.load_dataset("keertanavc/imdb_prefix20_forDPO_multi-preference_small", split=split, cache_dir=cache_dir)
+    dataset = datasets.load_dataset("keertanavc/imdb_prefix20_forDPO_multi-preference", split=split, cache_dir=cache_dir)
     print('done')
     def split_prompt_and_responses(ex):
         row_data = {}
